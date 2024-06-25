6bbd53ef2207990174bfe426d02f671ad59aa54f
long020806 <cstslswust@163.com>
2024-06-25 15:22:15 +0800

    first commit

---
+++ vite-project/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server:{
+    port:3000,
+    host:"0.0.0.0",
+    hmr:true,
+    watch: { usePolling: true }
+  }
+})
